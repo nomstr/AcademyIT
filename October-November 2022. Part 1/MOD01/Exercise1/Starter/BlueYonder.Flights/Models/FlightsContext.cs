c373f61d5e8124810ebe79a284f5a68c049a3e34
nomstr <bekkerveronica@yandex.ru>
2022-11-04 17:26:48 +0300

    add lab1 web-service

---
+++ October-November 2022. Part 1/MOD01/Exercise1/Starter/BlueYonder.Flights/Models/FlightsContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BlueYonder.Flights.Models
+    {
+         public class FlightsContext : DbContext
+    {
+        public FlightsContext(DbContextOptions<FlightsContext> options)
+                : base(options)
+                    {
+
+                    }
+                    public DbSet<Flight> Flights { get; set; }
+    }
+    }
+   ```
